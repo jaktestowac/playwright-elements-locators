7842453f89ddd916af32ebf31680d8d49e452874
Krzysiek <kkijas@jaktestowac.pl>
2024-08-24 15:11:00 +0200

    fix: auto waiting lessons

--- S01_selectors_and_locators/tests/L15_web_first_assertions/auto-waiting-base-template.spec.ts
+++ S01_selectors_and_locators/tests/L15_web_first_assertions/auto-waiting-base-template.spec.ts
@@ -5,13 +5,13 @@ test.describe.only("Web-first assertions and auto-waiting", () => {
     await page.goto("/practice/not-displayed-elements-1.html");
   });
 
-  test("Button visibility (Web-first assertions)", async ({ page }) => {
+  test("Auto-waiting for elements for action", async ({ page }) => {
     // Arrange:
     // Act:
     // Assert:
   });
 
-  test("Auto-waiting for elements for action", async ({ page }) => {
+  test("Button visibility (Web-first assertions)", async ({ page }) => {
     // Arrange:
     // Act:
     // Assert:
---
+++ S01_selectors_and_locators/tests/L15_web_first_assertions/auto-waiting.spec.ts
@@ -0,0 +1,45 @@
+import { test, expect } from "@playwright/test";
+
+test.describe.only("Web-first assertions and auto-waiting", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto("/practice/not-displayed-elements-1.html");
+  });
+
+  test("Auto-waiting for elements for action", async ({ page }) => {
+    // https://playwright.dev/docs/actionability
+    // Arrange:
+    const elementTestId = "dti-button-element";
+    const resultsTestId = "dti-results";
+    const expectedMessage = "You clicked the button!";
+
+    const buttonLocator = page.getByTestId(elementTestId);
+    const result = page.getByTestId(resultsTestId);
+
+    // print status of the button at this moment:
+    console.log("is button visible?", await buttonLocator.isVisible());
+
+    // Act:
+    // use auto-waiting during click:
+    await buttonLocator.click();
+
+    // Assert:
+    // and check the results:
+    await expect(result).toHaveText(expectedMessage);
+  });
+
+  test("Button visibility (Web-first assertions)", async ({ page }) => {
+    // https://playwright.dev/docs/best-practices#use-web-first-assertions
+    // Arrange:
+    const elementSelector = "dti-button-element";
+
+    const buttonLocator = page.getByTestId(elementSelector);
+
+    // print status of the button at this moment:
+    console.log("is button visible?", await buttonLocator.isVisible());
+
+    // Assert:
+    // use web-first assertions:
+    // this will automatically wait for the button to be visible:
+    await expect(buttonLocator).toBeVisible();
+  });
+});
